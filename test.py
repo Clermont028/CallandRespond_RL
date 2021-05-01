171f2516770743f95db4d3d34792b997d35a8ef7
alyPham <apham3@macalester.edu>
2021-05-01 18:59:03 -0500

    bare bones

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from music21 import *
+# configure.run()
+import numpy as np
 
 class callAndResponse():
     def __init__(self, initCall):
@@ -6,18 +8,70 @@ class callAndResponse():
         self.measure = self.call[0]
         self.call.pop(0)
 
-        self.initScore = self.score(self.call)
-        print(self.initScore)
+        self.initScore = self.calculate_score(self.call)
+        # print(self.initScore)
+        self.algorithm(self.call)
 
-    def score(self, measure):
+    def stochastic_note(self, note):
+        possible_notes = ['f4', 'g4', 'a', 'b', 'c', 'd', 'e', 'f', 'g5', 'a5', 'b5']
+
+        if 'a':
+            return np.random.choice(possible_notes, 1, p=[0.11, 0.11, 0.11, 0.11, 0.11, 0.075, 0.075, 0.075, 0.075, 0.075, 0.075])
+        if 'b':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.11, 0.11, 0.11, 0.11, 0.11, 0.075, 0.075, 0.075, 0.075, 0.075])
+        if 'c':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.075, 0.11, 0.11, 0.11, 0.11, 0.11, 0.075, 0.075, 0.075, 0.075])
+        if 'd':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.075, 0.075, 0.11, 0.11, 0.11, 0.11, 0.11, 0.075, 0.075, 0.075])
+        if 'e':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.075, 0.075, 0.075, 0.11, 0.11, 0.11, 0.11, 0.11, 0.075, 0.075])
+        if 'f':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.075, 0.075, 0.075, 0.075, 0.11, 0.11, 0.11, 0.11, 0.11, 0.075])
+        if 'g':
+            return np.random.choice(possible_notes, 1, p=[0.075, 0.075, 0.075, 0.075, 0.075, 0.075, 0.11, 0.11, 0.11, 0.11, 0.11])
+
+        
+    def choose_rhythm(self):
+        return np.random.choice(['4', '8', '16'])
+
+    def change(self, note):
+       
+        note = stochastic_note(note)
+        rhythm = choose_rhythm()
+        new_note = note + rhythm
+
+        return new_note
+
+    def algorithm(self, call):
+        improved_score = self.initScore
+        call = self.call
+        response = []
+
+        while improved_score > 1:
+            for note in call:
+                new_note = change(note)
+                response
+
+            response_score = calculate_score(response)
+            improved_score = response_score - improved_score
+            call = response
+            response = []
+
+        return response
+
+                
+    def calculate_score(self, measure):
         score = 0
         for i in range(len(measure) - 1):
             neighbor = ord(measure[i+1])   
             compare = ord(measure[i][0])
             print(compare - neighbor)
             if abs(compare - neighbor) > 2:
-                score -= 1
+               score -= 1
         return score
+
+    def getScore():
+        return self.initScore
 
 def main():
     call_str = input("Enter a measure: ")
@@ -25,7 +79,15 @@ def main():
     call = call_str.split(" ")
     print(call)
 
-    callAndResponse(call)
+    response = callAndResponse(call)
+    new_string = "tinynotation: 3/4"
+
+    for note in response:
+        new_string = new_string + " " + note
+
+    melody = converter.parse(new_string)
+    melody.show()
+
 
 if __name__ == "__main__" :
     main()
